e8e3f16b39dfaacd5a7a349b68dccf16e2d6d698
A-KAlqadsi <abdulkareemalqa@gmail.com>
2025-05-08 16:51:56 +0300

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+	public class TestController : Controller
+	{
+		public IActionResult Index()
+		{
+			TestViewModel testViewModel = new TestViewModel
+			{
+				Name = "Abdulkareem",
+				Age = 20
+			};
+
+			return View(testViewModel);
+		}
+	}
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+	public class TestViewModel
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
